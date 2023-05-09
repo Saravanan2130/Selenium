8bc9e7fb9717e8c5f579c417ef80a6e526d151b8
Saravanakannan <132178934+Saravanan2130@users.noreply.github.com>
2023-05-09 22:55:40 +0530

    Add files via upload

---
+++ Week 1/Day1.Assignment1/IsPrime.java
@@ -0,0 +1,27 @@
+package week1.assignment1;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+	
+		int n= 5;
+		boolean isPrime=true;
+
+			for (int i = 2; i <n; i++) 
+			{
+				if(n%i==0) 
+				{
+					isPrime=false;
+				    }
+			}
+			
+			if(isPrime=true)
+				
+			System.out.println("The entered number  "+n+"  is a prime number");
+			
+			else
+				
+			System.out.println("The entered number  "+n+"  is not a prime number");
+	}
+
+}
